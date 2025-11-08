49be1d997db6f026552fd0d9824defb6e8d01f1e
carherval <carlos.hernandezvalero@gmail.com>
2025-11-08 18:35:49 +0100

    Cambio de referencia de la imagen del "loader"

--- Frontend/src/components/loader/loader.js
+++ Frontend/src/components/loader/loader.js
@@ -1,9 +1,11 @@
 import './loader.css'
 
+import loader from '../../../assets/images/loader.png'
+
 const LOADING_MSG = 'Espere, por favor...'
 
 export const getLoader = () =>
-  `<div id="loader" class="flex oculto"><div class="flex loader"><img src="/assets/images/loader.png" alt="${LOADING_MSG}" /><p class="negrita">${LOADING_MSG}</p></div></div>`
+  `<div id="loader" class="flex oculto"><div class="flex loader"><img src="${loader}" alt="${LOADING_MSG}" /><p class="negrita">${LOADING_MSG}</p></div></div>`
 
 export const showLoader = () => {
   document.getElementById('loader').classList.remove('oculto')
